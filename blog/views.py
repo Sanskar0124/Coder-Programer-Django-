d9a43c077fcbfd03aea4118351ef7ea599f272b5
Sanskar <sansakhraliya@gmail.com>
2021-04-13 20:18:41 +0530

    functions to render templates

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def blogHome(request):
-    return HttpResponse('This is blogHome. We will keep all the blogpost here.')
+    return render(request, 'blog/blogHome.html')
 
 def blogPost(request, slug):
-    return HttpResponse(f'This is blogPost: {slug}')
+    return render(request, 'blog/blogPost.html')
