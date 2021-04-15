eac84fd9775ce97f09ed987333dc3a6e442981f5
Sanskar <sansakhraliya@gmail.com>
2021-04-15 23:22:32 +0530

    Added messages framework and pages

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -15,6 +16,11 @@ def contact(request):
         phone = request.POST.get('phone')
         content = request.POST.get('content')
         print(name, email,phone, content)
-        contact = Contact(name=name, email=email,phone=phone, content=content)
-        contact.save()
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form Correctly")
+        else:
+            contact = Contact(name=name, email=email,phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
     return render(request, 'home/contact.html') 
