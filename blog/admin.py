f884eb291532607338b48876ea8ca21d4d40e099
Sanskar0124 <sansakhraliya@gmail.com>
2021-10-23 08:30:02 +0530

    Minor Changes

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from blog.models import Post, BlogComment
 
 # Register your models here.
 admin.site.register((BlogComment))
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     class Media:
